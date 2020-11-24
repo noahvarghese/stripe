0764df7834b142d654eb2618a37705642c316474
Noah Varghese <noah.varghese@websan.com>
2020-11-24 13:39:18 -0500

    moved user to sequelize model

--- lib/models/User.ts
+++ lib/models/User.ts
@@ -1,56 +1,36 @@
-import e from "express";
+import {Sequelize, DataTypes} from "sequelize";
 import AbstractModel from "./AbstractModel";
 
-export class User implements AbstractModel {
+const sequelize = new Sequelize('sqlite::memory');
+
+export const User = sequelize.define("User", {
     // set initital values so that keys can be obtained
-    ID?: number = -1;
-    firstName: string = "";
-    lastName?: string = "";
-    email?: string = "";
-    hash?: string = "";
-    birthDate?: Date = new Date(8640000000000000);
-    phone?: number = -1;
-
-    constructor() {}
-
-    getProperties = (): { Name: string; Type: string }[] =>
-        Object.entries(this)
-            .filter(([key, _], __: any) =>
-                !["getProperties", "getKeys", "getListKeys"].includes(key)
-                    ? true
-                    : false
-            )
-            .map(([key, value], ___: number) => {
-                let type: string = typeof value;
-                // console.log(`Key: ${key}, Type: ${type}, Value: ${value}`);
-
-                if (type === "number") {
-                    if (value % 1 === 0) {
-                        type = "int";
-                    } else {
-                        type = "float";
-                    }
-                }
-
-                if (type === "object") {
-                    console.log(value instanceof Date);
-                    if (value instanceof Date) {
-                        type = "date";
-                    }
-                }
-
-                return {
-                    Name: key,
-                    Type: type,
-                };
-            });
-
-    getKeys = (): { Name: string; Type: string }[] => {
-        return [
-            { Name: "ID", Type: "number" },
-            { Name: "email", Type: "string" },
-        ];
-    };
-
-    getListKeys = () => [];
-}
+    ID: {
+        type: DataTypes.NUMBER,
+        allowNull: false
+    },
+    firstName: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    lastName: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    hash: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    birthDate: {
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+    phone: {
+        type: DataTypes.NUMBER,
+        allowNull: false
+    }
+});
